48b179775ba9272a34a9d1aecd17e757e26a803a
wanjocharles <wanjocharles98@gmail.com>
2024-01-22 01:17:29 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,9 +7,9 @@
  */
 void swap_ints(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+int tmp = *a;
+*a = *b;
+*b = tmp;
 }
 
 /**
@@ -21,24 +21,24 @@ void swap_ints(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, len = size;
-    bool bubbly = false;
+size_t i, len = size;
+bool bubbly = false;
 
-    if (array == NULL || size < 2)
-        return;
+if (array == NULL || size < 2)
+return;
 
-    while (!bubbly)
-    {
-        bubbly = true;
-        for (i = 0; i < len - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                swap_ints(array + i, array + i + 1);
-                print_array(array, size);
-                bubbly = false;
-            }
-        }
-        len--;
-    }
+while (!bubbly)
+{
+bubbly = true;
+for (i = 0; i < len - 1; i++)
+{
+if (array[i] > array[i + 1])
+{
+swap_ints(array + i, array + i + 1);
+print_array(array, size);
+bubbly = false;
+}
+}
+len--;
+}
 }
